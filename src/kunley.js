5d4c6440a3a08539c10000b5be0552d83f977d24
Kahlil Lechelt <hello@kahlil.info>
2019-08-08 08:53:50 +0200

    remove getters

--- src/kunley.js
+++ src/kunley.js
@@ -7,33 +7,25 @@ export class Kunley {
     initialAction = { type: 'INIT', reducer: state => state },
     debug = false
   ) {
-    this._action$ = new BehaviorSubject(initialAction);
-    this._state$ = new BehaviorSubject(initialState);
+    this.action$ = new BehaviorSubject(initialAction);
+    this.state$ = new BehaviorSubject(initialState);
     const applyReducer = scan(
       (state, action) => (action.reducer ? action.reducer(state) : state),
       initialState
     );
-    const pipeIntoState$ = state => this._state$.next(state);
-    this._action$.pipe(applyReducer).subscribe(pipeIntoState$);
-    this._action$.subscribe(action => (debug ? { action } : undefined));
-    this._state$.subscribe(state => (debug ? { state } : undefined));
+    const pipeIntoState$ = state => this.state$.next(state);
+    this.action$.pipe(applyReducer).subscribe(pipeIntoState$);
+    this.action$.subscribe(action => (debug ? { action } : undefined));
+    this.state$.subscribe(state => (debug ? { state } : undefined));
   }
 
   dispatch(action) {
-    this._action$.next(action);
-  }
-
-  get state$() {
-    return this._state$;
-  }
-
-  get action$() {
-    return this._action$;
+    this.action$.next(action);
   }
 
   runSideEffects(...sideEffects) {
     sideEffects.map(sideEffect => {
-      sideEffect(this._action$).subscribe(action => this._action$.next(action));
+      sideEffect(this.action$).subscribe(action => this.action$.next(action));
     });
   }
 }
